cf24b0d90d0b87dfbb6c54ea47129d34c19711a1
Zhehao Mao <zhehao.mao@gmail.com>
2012-12-22 14:53:52 -0500

    add pthread version

--- c/fourier.c
+++ c/fourier.c
@@ -1,15 +1,28 @@
 #include "fourier.h"
 #include <stdlib.h>
-#include <string.h>
+
+#ifdef FFT_PTHREAD
+#include <pthread.h>
+#include <unistd.h>
+
+struct fft_thread {
+	pthread_t thread;
+	double complex *fdom;
+	double complex *tdom;
+	int n;
+	int s;
+	int nthreads;
+};
+
+#endif
 
 void dft(double complex *fdom, double complex *tdom, int n)
 {
-	int memsize = n * sizeof(double complex);
 	int i, j;
 
-	memset(fdom, 0, memsize);
-
 	for (i = 0; i < n; i++) {
+		fdom[i] = 0;
+
 		for (j=0; j < n; j++) {
 			fdom[i] += tdom[j] * cexp(EXPCONST * i * j / n);
 		}
@@ -34,10 +47,79 @@ static void fft_r(double complex *fdom, double complex *tdom, int n, int s)
 	}
 }
 
+#ifdef FFT_PTHREAD
+
+static void* fft_par(void *data)
+{
+	struct fft_thread *oldthread;
+	struct fft_thread newthread;
+	int i, n, hn, s, nthreads;
+	double complex *fdom;
+
+	oldthread = (struct fft_thread *) data;
+	
+	nthreads = oldthread->nthreads;
+	n = oldthread->n;
+	hn = oldthread->n / 2;
+	s = oldthread->s;
+	fdom = oldthread->fdom;
+
+	if (n == 1) {
+		fdom[0] = oldthread->tdom[0];
+		return NULL;
+	}
+
+	if (nthreads == 1) {
+		fft_r(oldthread->fdom, oldthread->tdom, n, s);
+		return NULL;
+	}
+
+	
+	newthread.fdom = oldthread->fdom + hn;
+	newthread.tdom = oldthread->tdom + s;
+	newthread.n = hn;
+	newthread.s = 2 * s;
+	newthread.nthreads = nthreads / 2;
+
+	oldthread->n = hn;
+	oldthread->s = 2 * s;
+	oldthread->nthreads = nthreads / 2;
+
+	pthread_create(&newthread.thread, NULL, fft_par, (void*) &newthread);
+	fft_par((void*) oldthread);
+	
+	pthread_join(newthread.thread, NULL);
+
+	for (i = 0; i < hn; i++) {
+		double complex odd = fdom[i+hn];
+		fdom[i+hn] = fdom[i] - cexp(EXPCONST * i / n) * odd;
+		fdom[i] = fdom[i] + cexp(EXPCONST * i / n) * odd;
+	}
+
+	return NULL;
+}
+
+inline void fft(double complex *fdom, double complex *tdom, int n)
+{
+	struct fft_thread thread;
+	
+	thread.fdom = fdom;
+	thread.tdom = tdom;
+	thread.n = n;
+	thread.s = 1;
+	thread.nthreads = sysconf(_SC_NPROCESSORS_ONLN);
+
+	fft_par((void*) &thread);
+}
+
+#else
+
 inline void fft(double complex *fdom, double complex *tdom, int n)
 {
 	fft_r(fdom, tdom, n, 1);
 }
+
+#endif
 
 void ifft(double complex *tdom, double complex *fdom, int n)
 {
